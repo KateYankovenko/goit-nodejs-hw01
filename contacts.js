0fdab1603ca92b2ad1de8d8ea3d2eb9f5b64274a
Kate Yankovenko <k.yankovenko@ukr.net>
2022-10-08 15:43:14 +0200

    main fncs added, used promises

--- contacts.js
+++ contacts.js
@@ -1,22 +1,49 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
+const { uuid } = require('uuidv4');
 
+const filePath = path.join(__dirname, "db/contacts.json");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
+async function listContacts() {
+    const data = await fs.readFile(filePath);
+    const contacts = JSON.parse(data);
+    return contacts;
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+    const allContacts = await listContacts();
+    const contact = allContacts.find(contact => contact.id === String(contactId));
+    if (!contact) {
+        return null;
+    }
+    return contact;
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+    const allContacts = await listContacts();
+    const index = allContacts.findIndex(contact => contact.id === String(contactId))
+    if (index === -1) {
+        return null;
+    }
+    const updatedContacts = allContacts.filter(contact => contact.id !== String(contactId));
+    const deletedContact = allContacts[index];
+    await fs.writeFile(filePath, JSON.stringify(updatedContacts));
+    
+    return deletedContact;
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
+async function addContact(name, email, number) {
+    const allContacts = await listContacts();
+    const newContact = { name, email, number, id: uuid() };
+    allContacts.push(newContact);
+    await fs.writeFile(filePath, JSON.stringify(allContacts));
+    
+    return newContact;
 }
 
-module.exports = contacts;
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+}
